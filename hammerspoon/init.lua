f01001d0c955c7b02ee04f593a8ea52865c928b1
Henry Firth <henrynf12@gmail.com>
2020-04-29 23:20:53 -0500

    Add a center hotkey for hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,20 +1,23 @@
 local padding = 12
 local mash = { 'ctrl', 'alt', 'cmd' }
 
-function move(x, y, w, h)
+function move(x, y, w, h, p)
+  p = p or padding
+
   local win = hs.window.focusedWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
 
-  f.x = max.x + (max.w * x) + padding
-  f.y = max.y + (max.h * y) + padding
-  f.w = max.w * w - (padding * 2)
-  f.h = max.h * h - (padding * 2)
+  f.x = max.x + (max.w * x) + p
+  f.y = max.y + (max.h * y) + p
+  f.w = max.w * w - (p * 2)
+  f.h = max.h * h - (p * 2)
 
   win:setFrame(f)
 end
 
+function center()          move(0, 0, 1, 1, 42) end
 function maximize()        move(0, 0, 1, 1) end
 function topHalf()         move(0, 0, 1, 0.5) end
 function leftHalf()        move(0, 0, 0.5, 1) end
@@ -25,14 +28,15 @@ function topRightHalf()    move(0.5, 0, 0.5, 0.5) end
 function bottomRightHalf() move(0.5, 0.5, 0.5, 0.5) end
 function bottomLeftHalf()  move(0, 0.5, 0.5, 0.5) end
 
+hs.hotkey.bind(mash, "C", center)
+hs.hotkey.bind(mash, "M", maximize)
+hs.hotkey.bind(mash, "Up", topHalf)
 hs.hotkey.bind(mash, "Left", leftHalf)
 hs.hotkey.bind(mash, "Right", rightHalf)
-hs.hotkey.bind(mash, "Up", topHalf)
 hs.hotkey.bind(mash, "Down", bottomHalf)
 hs.hotkey.bind(mash, "1", topLeftHalf)
 hs.hotkey.bind(mash, "2", topRightHalf)
 hs.hotkey.bind(mash, "3", bottomRightHalf)
 hs.hotkey.bind(mash, "4", bottomLeftHalf)
-hs.hotkey.bind(mash, "M", maximize)
 
 hs.window.animationDuration = 0 -- disable animations
