1e40c7fc1e4b7290f0e75fb1cc7fe4d30fb1c7dd
Henry Firth <henrynf12@gmail.com>
2021-05-20 12:49:05 -0500

    Add some more nvim customizations

--- neovim/init.lua
+++ neovim/init.lua
@@ -3,6 +3,18 @@ u = require('utils')
 -- Enable syntax first so `hi` cmds work
 u.cmd('syntax on')
 
+-- Gruvbox baby
+u.plugin 'gruvbox-community/gruvbox'
+
+u.cmd('colorscheme gruvbox')
+u.cmd('hi GruvboxRedSign        ctermfg=167 ctermbg=NONE')
+u.cmd('hi GruvboxGreenSign      ctermfg=142 ctermbg=NONE')
+u.cmd('hi GruvboxYellowSign     ctermfg=214 ctermbg=NONE')
+u.cmd('hi GruvboxBlueSign       ctermfg=109 ctermbg=NONE')
+u.cmd('hi GruvboxPurpleSign     ctermfg=175 ctermbg=NONE')
+u.cmd('hi GruvboxAquaSign       ctermfg=108 ctermbg=NONE')
+u.cmd('hi GruvboxOrangeSign     ctermfg=208 ctermbg=NONE')
+
 -- Syntax highlighting
 u.plugin 'elixir-editors/vim-elixir'
 
@@ -19,10 +31,13 @@ u.plugin 'tpope/vim-rhubarb'
 u.plugin 'tpope/vim-repeat'
 u.plugin 'tpope/vim-surround'
 
--- Gruvbox baby
-u.plugin 'gruvbox-community/gruvbox'
+-- Stylish GitGutter
+u.plugin 'airblade/vim-gitgutter'
 
-u.cmd('colorscheme gruvbox')
+u.cmd('hi GitGutterAdd          ctermfg=142')
+u.cmd('hi GitGutterChange       ctermfg=214')
+u.cmd('hi GitGutterDelete       ctermfg=167')
+u.cmd('hi GitGutterChangeDelete ctermfg=208')
 
 -- Relative line numbers
 u.set_window('number', true)
@@ -34,3 +49,16 @@ u.cmd('hi CursorLineNR ctermbg=NONE')
 u.set_window('signcolumn', 'yes')
 
 u.cmd('hi clear SignColumn')
+
+-- Mouse settings
+u.set_global('mouse', 'a')
+u.set_global('scrolloff', 8)
+
+-- Fancy whitespace characters
+u.set_window('list', true)
+u.set_window('listchars', 'trail:·,tab:>-')
+
+u.cmd('hi SpecialKey ctermfg=59 guifg=#5f5f5f')
+
+-- Search preferences
+u.set_global('incsearch', true)
