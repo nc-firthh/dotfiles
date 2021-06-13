b9262b7dfc97493fd5b12caf7067dba14983dcb0
Henry Firth <henrynf12@gmail.com>
2021-06-13 10:37:27 -0600

    Double-comment lsp headers

--- neovim/lsp.lua
+++ neovim/lsp.lua
@@ -4,7 +4,7 @@ local nvim_lsp = require('lspconfig')
 
 -- Uncomment individual configs only once corresponding LS is installed
 
--- ElixirLS
+-- -- ElixirLS
 -- nvim_lsp.elixirls.setup{
 -- 	cmd = { "/Users/henryfirth/.config/lsp/elixir-ls/language_server.sh" };
 -- 	on_attach = u.on_attach;
@@ -13,22 +13,22 @@ local nvim_lsp = require('lspconfig')
 -- 	};
 -- }
 
--- elmls
+-- -- elmls
 -- nvim_lsp.elmls.setup{
 -- 	on_attach = u.on_attach;
 -- }
 
--- tsserver
+-- -- tsserver
 -- nvim_lsp.tsserver.setup{
 -- 	on_attach = u.on_attach;
 -- }
 
--- gopls
+-- -- gopls
 -- nvim_lsp.gopls.setup{
 -- 	on_attach = u.on_attach;
 -- }
 
--- rust-analyzer
+-- -- rust-analyzer
 -- nvim_lsp.rust_analyzer.setup{
 -- 	on_attach = u.on_attach;
 -- }
