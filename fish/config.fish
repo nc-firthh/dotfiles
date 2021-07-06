d94c560b2d30dedd8424395bedb6ad58bbb91e90
Henry Firth <firthh@nextcapital.com>
2021-07-06 09:38:26 -0500

    Add helper functions and shortcuts to fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,30 @@ set PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
 
 # Set up asdf-vm
 source ~/.asdf/asdf.fish
+
+# Set EDITOR and VISUAL
+set EDITOR nvim
+set VISUAL nvim
+
+# Bind CTRL+e to editor
+bind \ce edit_command_buffer
+
+# Source .profile
+bass source ~/.profile
+
+# Add function for cheat.sh
+function tldr
+	curl -s cht.sh/$argv | less -RF
+end
+
+function semgrep_audit
+	docker run --rm -v "$PWD:/src" \
+		returntocorp/semgrep \
+		--config=p/r2c-security-audit \
+		--exclude='spec/' \
+		--exclude='db/migrate' \
+		--exclude='db/auditing/migrate' \
+		--exclude='public/javascripts' \
+		--exclude='yard_template/default/fulldoc/html/js' \
+		~/code/appserver
+end
